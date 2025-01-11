56c408448e5f1f54e1f12d7ab3088119c4dde9e1
krish-nakrani-2002 <nakranikrish5@gmail.com>
2025-01-11 18:59:08 +0530

    food project setup

---
+++ foodmood/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Success from './pages/Success'
+
+function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default App
