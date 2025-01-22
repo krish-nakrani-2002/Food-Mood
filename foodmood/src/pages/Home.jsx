0b52e68a8a21eb20e6c66e629b300ff5bde4155f
krish-nakrani-2002 <nakranikrish5@gmail.com>
2025-01-22 16:10:36 +0530

    create categorymenu buttons

--- foodmood/src/pages/Home.jsx
+++ foodmood/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../Components/Nav'
+import CategoryMenu from '../Components/CategoryMenu'
 
 function Home() {
   return (
     <>
       <Nav />
+      <CategoryMenu />
     </>
   )
 }
