6d04e64abc7b3947e1b570ce12bdde9034ae76ac
krish-nakrani-2002 <nakranikrish5@gmail.com>
2025-01-23 18:53:36 +0530

    create itemcard in cart

--- foodmood/src/Components/Cart.jsx
+++ foodmood/src/Components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { IoMdCloseCircle } from "react-icons/io";
+import { AiOutlineClose  } from "react-icons/ai";
+import ItemCard from './ItemCard';
 
 const Cart = () => {
   return (
@@ -7,8 +8,11 @@ const Cart = () => {
     <div className='fixed top-0 right-0 w-full lg:w-[21vw] h-full p-5 bg-white'>
         <div className='flex justify-between my-3'>
             <span className='text-xl font-bold text-gray-800'>My Order</span>
-            <IoMdCloseCircle className='border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-400 hover:border-red-400 cursor-pointer' />
+            <AiOutlineClose  className='border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-400 hover:border-red-400 cursor-pointer' />
         </div>
+
+        <ItemCard />
+
         <div className='absolute bottom-0'> 
             <h3 className='font-semibold text-gray-800'>Items: </h3>
             <h3 className='font-semibold text-gray-800'>Total Amount: </h3>
---
+++ foodmood/src/Components/ItemCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+
+const ItemCard = () => {
+  return (
+    <div className='flex gap-2 shadow-md rounded-lg p-2 mb-3'>
+        <MdDelete className='absolute right-7 text-gray-600 cursor-pointer' />
+        <img src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5" 
+        alt=""
+        className='w-[50px] h-[50px]' 
+        />
+        <div className='leading-5'>
+          <h2 className='font-bold text-gray-800'>Onion Pizza</h2>
+          <div className='flex justify-between'>
+            <span className='text-green-500 font-bold'>₹120</span>
+            <div className='flex gap-2 items-center justify-center absolute right-7'>
+          <AiOutlinePlus className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-xl transition-all ease-linear cursor-pointer hover:border-none' /> 
+          <span>1</span>
+          <AiOutlineMinus className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-xl transition-all ease-linear cursor-pointer hover:border-none' />
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ItemCard
