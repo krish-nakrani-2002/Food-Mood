62de666cdf755792fb6f1932faa337c21e5ff832
krish-nakrani-2002 <nakranikrish5@gmail.com>
2025-01-26 16:54:28 +0530

    Extracting unique categories

--- foodmood/src/Components/CategoryMenu.jsx
+++ foodmood/src/Components/CategoryMenu.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import FoodData from '../Data/FoodData';
 
-function CategoryMenu() {
+const CategoryMenu = () => {
+  const [categories, setCategories] = useState([]);
+  
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category))];
+    setCategories(uniqueCategories);
+  };
+
+  useEffect(() => {
+    listUniqueCategories();
+  }, [])
+  
+
   return (
     <div className='ml-6'>
         <h3 className='text-xl font-semibold'>Find the best food</h3>
         <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden'>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>
-            All</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>
-            Lunch</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>
-            Breakfast</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>
-            Dinner</button>
-            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>
-            Snacks</button>
+            
+            {
+              categories.map((category, index) => {
+                return (
+                  <button
+                   key={index} 
+                   className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>
+                    {category}
+                  </button>
+                )
+              }
+            )}
 
         </div>
     </div>
